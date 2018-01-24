7812101e3a9688f7dc406061091c746792c42093
Git Faf <git.faf@gmail.com>
2018-01-24 21:15:07 +0530

    Version 2

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = (input, n) => input.split('').filter(i => i.charCodeAt(0) >= 32 && i.charCodeAt(0) <= 127).slice(0, n).join('')
+module.exports = (input, n) => String(input).split('').filter(i => i.charCodeAt(0) >= 32 && i.charCodeAt(0) <= 127).slice(0, n).join('')
